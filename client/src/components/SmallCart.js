d5018da2ac1cbde404f4403be7a8cb12cdf87dd8
Alexandre <alex_18h@hotmail.com>
2021-02-06 15:53:51 -0500

    Small cart component

--- client/src/components/SmallCart.js
+++ client/src/components/SmallCart.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const SmallCart = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <div></div>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 300px;
+  height: 100%;
+  border: solid 2px black;
+`;
 
 export default SmallCart;
