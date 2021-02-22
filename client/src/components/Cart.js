f67b3c6608bc87946db52d4dca8627ccd5ee2af8
Vanessa Chan <chw.vanessa@gmail.com>
2021-02-22 12:08:18 -0500

    Cart.js - Added Link instead of Purchase Button

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -6,77 +6,79 @@ import styled from 'styled-components';
 import CartItem from './CartItem';
 
 const Cart = () => {
-    const storeItems = useSelector((state) => state);
-//     let total = 0;
-//   storeItems.map((item) => (total += item.quantity * item.price));
-//   let formatedPrice = new Intl.NumberFormat("en-US", {
-//     style: "currency",
-//     currency: "USD",
-//   }).format(total / 100);
+  const storeItems = useSelector((state) => state);
+  //     let total = 0;
+  //   storeItems.map((item) => (total += item.quantity * item.price));
+  //   let formatedPrice = new Intl.NumberFormat("en-US", {
+  //     style: "currency",
+  //     currency: "USD",
+  //   }).format(total / 100);
 
-    return (
+  return (
     <Wrapper>
-        <Title>Shopping Cart</Title>
-        <Container>
-            {storeItems && 
-            Object.values(storeItems).map((item) => {
-            return(
-                <CartItem 
-                    key={item.id}
-                    item={item}
-                    id={item._id}
-                    quantity={item.quantity}
-                /> );
-            })
-            }
-            <PurchasingSection>
-            <PurchaseButton>Purchase</PurchaseButton>
-                <Total>
-                    Total: <b>$0.00</b>
-                    {/* {total > 0 ? <b>{formatedPrice}</b> : <b>$0.00</b>} */}
-                </Total>
-                
-            </PurchasingSection>
-        </Container>
+      <Title>Shopping Cart</Title>
+      <Container>
+        {storeItems &&
+          Object.values(storeItems).map((item) => {
+            return (
+              <CartItem
+                key={item.id}
+                item={item}
+                id={item._id}
+                quantity={item.quantity}
+              />
+            );
+          })}
+        <PurchasingSection>
+          <PurchaseButton
+            to={Object.values(storeItems).length > 0 ? '/purchase' : '/cart'}
+          >
+            Purchase
+          </PurchaseButton>
+          <Total>
+            Total: <b>$0.00</b>
+            {/* {total > 0 ? <b>{formatedPrice}</b> : <b>$0.00</b>} */}
+          </Total>
+        </PurchasingSection>
+      </Container>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-    margin:auto;
-    padding:50px;
-    width: 100%;
-    
-    justify-content:center;
+  margin: auto;
+  padding: 50px;
+  width: 100%;
+
+  justify-content: center;
 `;
 
 const Container = styled.div`
-    width:100%;
-    
+  width: 100%;
 `;
 
 const Title = styled.h2`
-    border-bottom:2px solid lightgray;
-    padding-bottom:10px;
-    
+  border-bottom: 2px solid lightgray;
+  padding-bottom: 10px;
 `;
 
 const PurchasingSection = styled.div`
-    display:flex;
-    justify-content:flex-end;
-    
-    
+  display: flex;
+  justify-content: flex-end;
 `;
 
-const PurchaseButton = styled.button`
-    width: 120px;
-    height:30px;
-    margin: 0px 10px;
-    `;
+const PurchaseButton = styled(Link)`
+  width: 120px;
+  height: 30px;
+  margin: 0px 10px;
+  border: 1px solid black;
+  text-decoration: none;
+  text-align: center;
+`;
 
 const Total = styled.div`
-    display:flex;
-    font-size: 22px;
+  display: flex;
+  font-size: 22px;
 `;
 
 export default Cart;
